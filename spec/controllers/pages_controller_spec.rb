a396aec14d72081272cc6aa615fa2a9037d8d118
Peter Krnjevic <pkrnjevic@gmail.com>
2012-02-06 03:05:21 -0500

    Cleaned up routes

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -9,10 +9,10 @@ describe PagesController do
       response.should be_success
     end
     it "should have the right title" do
-          get 'home'
-          response.should have_selector("title",
-                            :content => "Ruby on Rails Tutorial Sample App | Home")
-        end
+      get 'home'
+      response.should have_selector("title",
+        :content => "Ruby on Rails Tutorial Sample App | Home")
+    end
   end
   
 
