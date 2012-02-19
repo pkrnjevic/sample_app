b7b0a3c5da87103193c70be77ded6a6aa0668c2f
Peter Krnjevic <pkrnjevic@gmail.com>
2012-02-19 18:25:23 -0500

    Interim 'following' commit

---
+++ spec/factories/relationships.rb
@@ -0,0 +1,9 @@
+# Read about factories at http://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :relationship do
+    follower_id 1
+    followed_id "MyString"
+    integer "MyString"
+  end
+end
