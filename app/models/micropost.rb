d8c5924076d7555d3b6e04674c18ba4d355c6986
Peter Krnjevic <pkrnjevic@gmail.com>
2012-02-20 23:51:08 -0500

    Pre-optimization using 'scopes, subselects, and a lambda'

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,4 +5,9 @@ class Micropost < ActiveRecord::Base
   validates :content, presence: true, length: { maximum: 140 }
   validates :user_id, presence: true
   default_scope order: 'microposts.created_at DESC'
+
+  def self.from_users_followed_by(user)
+    followed_user_ids = user.followed_user_ids.join(', ')
+    where("user_id IN (#{followed_user_ids}) OR user_id = ?", user)
+  end
 end
